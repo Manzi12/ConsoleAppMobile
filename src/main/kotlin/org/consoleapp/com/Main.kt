2c40efa9109627880f449dd1108110c778b8ff50
Manzi <manzeyjoseph@gmail.com>
2020-10-19 12:53:55 +0100

    Initial Commit. Created the bare bones of the project.

---
+++ src/main/kotlin/org/consoleapp/com/Main.kt
@@ -0,0 +1,59 @@
+package org.consoleapp.com
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main() {
+    logger.info { "Launching Task Scheduling Console App" }
+    println("Task Scheduling Kotlin App Version 1.0")
+
+    var input: Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> addTask()
+            2 -> updateTask()
+            3 -> listTasks()
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Task Scheduling Console App" }
+}
+
+fun menu() : Int {
+
+    val option : Int
+    var input: String? = null
+
+
+    println("MAIN MENU")
+    println(" 1. Add Task")
+    println(" 2. Update Task")
+    println(" 3. List All Tasks")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && input.isNotEmpty())
+        input.toInt()
+    else
+        -9
+    return option
+}
+
+fun addTask(){
+    println("You Chose Add Task")
+}
+
+fun updateTask() {
+    println("You Chose Update Task")
+}
+
+fun listTasks() {
+    println("You Chose List All Tasks")
+}
+
